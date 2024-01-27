2eb5b034778120a14d7f3bfd0e9788f6773fb682
FWasik <franekwasik2@gmail.com>
2024-01-27 14:06:28 +0100

    custom exception + handling category on delete #1

--- parts_warehouse_project/warehouse_app/views.py
+++ parts_warehouse_project/warehouse_app/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from .serializers import PartSerializer, CategorySerializer
 from .models import Part, Category
+from .exceptions import CustomException
 
 
 class PartViewSet(viewsets.ModelViewSet):
@@ -11,3 +12,16 @@ class PartViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def destroy(self, request, *args, **kwargs):
+        category = self.get_object()
+
+        parts_assigned = Part.objects.filter(category=category.name)
+
+        if parts_assigned:
+            raise CustomException('Cannot delete category that is assigned to parts', 400)
+
+        return super().destroy(request, args, kwargs)
+
+
+
