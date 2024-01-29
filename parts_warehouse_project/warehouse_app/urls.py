bb9f2b96f2b6a333ef4906cd0d52c7dca02a4b94
FWasik <franekwasik2@gmail.com>
2024-01-29 18:45:08 +0100

    adding search functionality + black

--- parts_warehouse_project/warehouse_app/urls.py
+++ parts_warehouse_project/warehouse_app/urls.py
@@ -1,12 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import PartViewSet, CategoryViewSet
+from .views import PartViewSet, PartSearchView, CategoryViewSet
 
-app_name = 'warehouse_app'
+app_name = "warehouse_app"
 
 router = routers.DefaultRouter()
-router.register(r'parts', PartViewSet, basename='parts')
-router.register(r'categories', CategoryViewSet, basename='categories')
+router.register(r"parts", PartViewSet, basename="parts")
+router.register(r"categories", CategoryViewSet, basename="categories")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("", include(router.urls)),
+    path("parts-search/", PartSearchView.as_view(), name="parts-search"),
+]
--- parts_warehouse_project/warehouse_app/views.py
+++ parts_warehouse_project/warehouse_app/views.py
@@ -1,12 +1,27 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .serializers import PartSerializer, CategorySerializer
 from .models import Part, Category
 from .exceptions import CustomException
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class PartViewSet(viewsets.ModelViewSet):
     queryset = Part.objects.all()
     serializer_class = PartSerializer
+
+
+class PartSearchView(generics.ListAPIView):
+    queryset = Part.objects.all()
+    serializer_class = PartSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = [
+        "serial_number",
+        "name",
+        "description",
+        "category",
+        "quantity",
+        "price",
+    ]
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
