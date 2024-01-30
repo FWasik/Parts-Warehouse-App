dd6ee60c5a9584b3bb0e8903c8e964b1d672aaab
FWasik <franekwasik2@gmail.com>
2024-01-30 22:39:02 +0100

    dropping unnecessary files and removing models from admin

--- parts_warehouse_project/warehouse_app/admin.py
+++ parts_warehouse_project/warehouse_app/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Part, Category
 
 
-admin.site.register(Part)
-admin.site.register(Category)
+
 
--- parts_warehouse_project/warehouse_app/exceptions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.exceptions import APIException
-
-class CustomException(APIException):
-    default_code = 500
-    default_detail = {'message': 'Error occurred'}
-
-    def __init__(self, message, status_code):
-        self.detail = {'message': message}
-        self.status_code = status_code
