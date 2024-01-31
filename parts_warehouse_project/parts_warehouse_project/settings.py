2158ea83270df3c3387fc100005bdd330f477a70
FWasik <franekwasik2@gmail.com>
2024-01-31 10:38:38 +0100

    adding docker + dropping secret key from .env

--- parts_warehouse_project/parts_warehouse_project/settings.py
+++ parts_warehouse_project/parts_warehouse_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "django-insecure-@0v16ck$132cc@sd(u69h#yx7%8g!m^a)z9i4w3bmt8$qcg6lh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
